708bc1930dee84133f44a8bd65bc6fd975851100
Ryan <221235059+rwilliamspbg-ops@users.noreply.github.com>
2026-05-15 01:38:20 +0000

    wip: continue AF_XDP test tweaks (reapply stash)

--- internal/routing/router_test.go
+++ internal/routing/router_test.go
@@ -1,47 +1,47 @@
 package routing
 
 import (
-    "crypto/rand"
-    "encoding/hex"
-    "testing"
+	"crypto/rand"
+	"encoding/hex"
+	"testing"
 )
 
 func randomID(t *testing.T) [32]byte {
-    var b [32]byte
-    if _, err := rand.Read(b[:]); err != nil {
-        t.Fatalf("rand read: %v", err)
-    }
-    return b
+	var b [32]byte
+	if _, err := rand.Read(b[:]); err != nil {
+		t.Fatalf("rand read: %v", err)
+	}
+	return b
 }
 
 func TestUpdateAndLookup(t *testing.T) {
-    tbl := NewTable()
-    dst := randomID(t)
-    nh := randomID(t)
-    tbl.UpdateRoute(RouteEntry{DestID: dst, NextHopID: nh})
+	tbl := NewTable()
+	dst := randomID(t)
+	nh := randomID(t)
+	tbl.UpdateRoute(RouteEntry{DestID: dst, NextHopID: nh})
 
-    got, ok := tbl.LookupNextHop(dst, 0)
-    if !ok {
-        t.Fatalf("expected route for %s", hex.EncodeToString(dst[:4]))
-    }
-    if got != nh {
-        t.Fatalf("unexpected next hop")
-    }
+	got, ok := tbl.LookupNextHop(dst, 0)
+	if !ok {
+		t.Fatalf("expected route for %s", hex.EncodeToString(dst[:4]))
+	}
+	if got != nh {
+		t.Fatalf("unexpected next hop")
+	}
 }
 
 func TestPredictive(t *testing.T) {
-    tbl := NewTable()
-    // Populate several entries
-    for i := 0; i < 8; i++ {
-        dst := randomID(t)
-        nh := randomID(t)
-        tbl.UpdateRoute(RouteEntry{DestID: dst, NextHopID: nh})
-    }
-    src := randomID(t)
-    dst := randomID(t)
-    nh, ok := tbl.PredictiveNextHop(src, dst, 42)
-    if !ok {
-        t.Fatalf("expected predictive next hop")
-    }
-    _ = nh // just ensure we returned something deterministic
+	tbl := NewTable()
+	// Populate several entries
+	for i := 0; i < 8; i++ {
+		dst := randomID(t)
+		nh := randomID(t)
+		tbl.UpdateRoute(RouteEntry{DestID: dst, NextHopID: nh})
+	}
+	src := randomID(t)
+	dst := randomID(t)
+	nh, ok := tbl.PredictiveNextHop(src, dst, 42)
+	if !ok {
+		t.Fatalf("expected predictive next hop")
+	}
+	_ = nh // just ensure we returned something deterministic
 }
